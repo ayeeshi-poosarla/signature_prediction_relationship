8157f80d0bcbda879eac3d5304fb643ecc34a686
AtticusColwell <atticusscott@icloud.com>
2025-04-09 16:50:11 -0400

    commented out non functioning code

--- data_download.R
+++ data_download.R
@@ -14,12 +14,12 @@ exp_df <- as.data.frame(SummarizedExperiment::assay(exp_data))
 write.csv(exp_df, "tcga_expression.csv")
 
 # code failed below here, new fix present on mut_data_download.R file
-query_mut <- GDCquery(
-  project = "TCGA-BRCA",
-  data.category = "Simple Nucleotide Variation",
-  data.type = "Masked Somatic Mutation"
-)
-GDCdownload(query_mut)
-mut_data <- GDCprepare(query = query_mut)
+#query_mut <- GDCquery(
+  #project = "TCGA-BRCA",
+  #data.category = "Simple Nucleotide Variation",
+  #data.type = "Masked Somatic Mutation"
+#)
+#GDCdownload(query_mut)
+#mut_data <- GDCprepare(query = query_mut)
 
-write.csv(mut_data, "tcga_mutations.csv")
+#write.csv(mut_data, "tcga_mutations.csv")
